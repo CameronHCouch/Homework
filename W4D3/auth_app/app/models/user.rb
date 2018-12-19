96003f40ca44ccfb82deed5b752a2bfdb03f07ab
CameronHCouch <cameronhcouch@gmail.com>
2018-12-18 22:13:26 -0500

    Complete User Auth HW

---
+++ W4D3/auth_app/app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank'}
+  validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  # searches user by username and returns User if user found and dehashed pass = inputted pass
+  def self.find_by_credentials(user, pword)
+    user = User.find_by(username: user)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(pword)
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+    # arg determines length of pseudorandom string
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def password=(pword)
+    @password = pword
+    self.password_digest = BCrypt::Password.new(user.password)
+  end
+end
