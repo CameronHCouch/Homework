38143401ba9cf3547373eb134ceb6067d9c600bd
CameronHCouch <cameronhcouch@gmail.com>
2018-12-26 18:09:51 -0500

    Add N+1 and Rails Intro exercises

--- W3D3/people_in_houses/db/migrate/20181212033734_create_people_table.rb
+++ W3D3/people_in_houses/db/migrate/20181212033734_create_people_table.rb
@@ -5,5 +5,6 @@ class CreatePeopleTable < ActiveRecord::Migration[5.2]
       t.integer :house_id, null: false
       t.timestamps
     end
+    add_index :people, :house_id
   end
 end
--- W3D3/people_in_houses/db/migrate/20181212034908_create_houses.rb
+++ W3D3/people_in_houses/db/migrate/20181212034908_create_houses.rb
@@ -2,7 +2,6 @@ class CreateHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :houses do |t|
       t.string :address, null: false
-      t.string :residents,
       t.timestamps
     end
   end
