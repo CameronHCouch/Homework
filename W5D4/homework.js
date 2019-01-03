1284d17ba1a0aa54f321baccde68e0af89825093
CameronHCouch <cameronhcouch@gmail.com>
2019-01-02 23:37:39 -0500

    Complete phase1 of JS Intro HW

--- W5D4/homework.js
+++ W5D4/homework.js
@@ -16,3 +16,37 @@ const fizzBuzz = (arr) => {
   }
   return result
 };
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0 && num !== 2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const firstNPrimes = (num) => {
+  let counter = 0
+  let result = []
+  for (let i = 2; counter < num ; i++) {
+    if (isPrime(i)) {
+      counter++;
+      result.push(i);
+    }
+  }
+  return result;
+};
+
+const sumOfNPrimes = (num) => {
+  let sum = 0
+  let primeArr = firstNPrimes(num);
+  for (let i = 0; i < primeArr.length; i++) {
+    sum += primeArr[i];
+  }
+  return sum;
+};
